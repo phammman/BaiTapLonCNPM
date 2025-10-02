de4b78765ea2bc2e5d9ccb791aa152b39db550b4
MDUNG99 <dungmanhtran203@gmai.com>
2025-10-02 10:23:53 +0700

    updata

--- BaiTapLonCNPM/Front-end/login/DangXuat.php
+++ BaiTapLonCNPM/Front-end/login/DangXuat.php
@@ -4,8 +4,7 @@ session_start();
 // Xóa toàn bộ session
 session_unset();
 session_destroy();
-
-// Quay về trang đăng nhập
+// Chuyển hướng về trang đăng nhập
 header("Location: DangNhap.php");
 exit;
 ?>
